e5b3daadbac6b7f581aa084180e85cf6057a33c0
William MARTIN <william.martin@hey.com>
2021-04-11 23:40:27 +0200

    Render shape is moving

--- lib/tetris/point.ex
+++ lib/tetris/point.ex
@@ -6,4 +6,8 @@ defmodule Tetris.Point do
   def left({x, y}), do: {x - 1, y}
   def right({x, y}), do: {x + 1, y}
   def down({x, y}), do: {x, y + 1}
+
+  def move({x, y}, {x_change, y_change}) do
+    {x + x_change, y + y_change}
+  end
 end
